a23b190ca16548266e8b1d072cb2b96bcde7fa3e
Kenn Sebesta <kenn@eissq.com>
2015-03-14 20:13:02 -0400

    Rotations: Fix incorrect reference to self.

--- flight/rotations.py
+++ flight/rotations.py
@@ -8,15 +8,15 @@ def euler_to_dcm(psi, theta, phi):
 	cPsi = np.cos(psi)
 	sPsi = np.sin(psi)
 	a1 = (cT*cPsi)
-	a2 = (np.sin(self.phi)*sT*cPsi-cPhi*sinPsi)
-	a3 = (cPhi*sT*cPsi+np.sin(self.phi)*sinPsi)
+	a2 = (sPhi*sT*cPsi-cPhi*sinPsi)
+	a3 = (cPhi*sT*cPsi+sPhi*sinPsi)
 
-	b1 = cT*np.sin(self.phi)
-	b2 = sinPsi*sT*np.sin(self.phi)+cPsi*cPhi
-	b3 = cPsi*sT*np.sin(self.phi)-sinPsi*cPhi
+	b1 = cT*sPhi
+	b2 = sinPsi*sT*sPhi+cPsi*cPhi
+	b3 = cPsi*sT*sPhi-sinPsi*cPhi
 
 	c1 = (-sT)
-	c2 = np.sin(self.phi)*cT
+	c2 = sPhi*cT
 	c3 = cPhi*cT
 
 	return  np.matrix('a1 a2 a3; b1 b2 b3; c1 c2 c3')
